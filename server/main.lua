8fec51fc15017aad8ff22bef15bba40e93d3aa89
wasabirobby <79250053+wasabirobby@users.noreply.github.com>
2023-01-06 01:34:05 -0800

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,11 +1,7 @@
+ESX = exports[Config.CoreFolderName]:getSharedObject()
 CreateThread(function()
-    if Config.OldQBCore then
-        TriggerEvent(Config.Core..':GetObject', function(obj) QBCore = obj end)
-    else
-        QBCore = exports[Config.CoreFolderName]:GetCoreObject()
-    end
-
     for k, v in pairs(Config.Books) do
+            
         QBCore.Functions.CreateUseableItem(k, function(source, item)
             TriggerClientEvent("gmm-books:client:OpenBook", source, k, item)
         end)
