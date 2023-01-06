3e966a8ee96b017356e4b4a77794b2080368f19a
wasabirobby <79250053+wasabirobby@users.noreply.github.com>
2023-01-06 01:36:29 -0800

    feat: esx

--- server/main.lua
+++ server/main.lua
@@ -1,9 +1,8 @@
 ESX = exports[Config.CoreFolderName]:getSharedObject()
 CreateThread(function()
     for k, v in pairs(Config.Books) do
-            
-        QBCore.Functions.CreateUseableItem(k, function(source, item)
-            TriggerClientEvent("gmm-books:client:OpenBook", source, k, item)
+        ESX.RegisterUsableItem(k, function(source)
+            TriggerClientEvent("gmm-books:client:OpenBook", source, k)
         end)
     end
 end)
